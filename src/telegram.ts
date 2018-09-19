9943416d2236de0f9910922af06700edabb8b0bd
Senya Pugach <upisfree@artlebedev.ru>
2018-09-19 07:28:02 +0300

    optimize audio sending to telegram by providing url, not buffer

--- src/telegram.ts
+++ src/telegram.ts
@@ -21,36 +21,15 @@ namespace Telegram {
 
     public sendUrlsToChannel(urls: VK.Audio[]): void {
       for (let i = 0; i < urls.length; i++) {
-        let file = fs.createWriteStream('tmp/' + i + '.mp3');
-        let get = https.get(urls[i].url, (res) => {
-          res.headers['Content-Type'] = 'multipart/form-data';
-          res.headers['Content-Disposition'] = `name*=UTF-8''${ encodeURIComponent(urls[i].artist + ' — ' + urls[i].title) }`;
+        let fileOptions: TelegramBot.SendAudioOptions = {
+          performer: urls[i].artist,
+          title: urls[i].title
+        };
 
-          res.pipe(file);
-
-          // console.log(res.headers)
-
-          // TODO: указывать прогресс загрузки. Пока есть контроль только над скачиванием файла к себе,
-          //       над загрузкой в телегу контроля нет (его нужно вынести из библиотеки).
-          // let fullLength = parseInt(res.headers['content-length'] || '');
-          // let received = 0;
-
-          // res.on('data', (d) => {
-          //   received += d.length;
-
-          //   console.log(Math.round(received / fullLength * 100) + '%');
-          // });
-
-          let fileOptions: TelegramBot.SendAudioOptions = {
-            performer: urls[i].artist,
-            title: urls[i].title,
-          };
-
-          this.botInstance.sendAudio(CONFIG.TELEGRAM.GROUP_URL, res, fileOptions).then((m) => {
-            log('[tg]', `send audio "${ urls[i].artist } — ${ urls[i].title }"`);
-          }, (e) => {
-            log('[tg]', 'sendUrlsToChannel() failed:', e);
-          });
+        this.botInstance.sendAudio(CONFIG.TELEGRAM.GROUP_URL, urls[i].url, fileOptions).then((m) => {
+          log('[tg]', `send audio "${ urls[i].artist } — ${ urls[i].title }"`);
+        }, (e) => {
+          log('[tg]', 'sendUrlsToChannel() failed:', e);
         });
       };
     }
